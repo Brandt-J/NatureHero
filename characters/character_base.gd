6100fafb97f30fb25dc1453112d7d2a537466476
Josef Brandt <josefbrandt@posteo.de>
2025-05-29 21:50:53 +0200

    Moving Placeholder Character

--- characters/character_base.gd
+++ characters/character_base.gd
@@ -7,5 +7,5 @@ extends Node2D
 
 
 func _process(delta: float) -> void:
-	position += gui.get_movement_vector() * speed * delta
-	rotation = gui.get_rotation_angle()
+	character_sprite.position += gui.get_movement_vector() * speed * delta
+	character_sprite.rotation = gui.get_rotation_angle()
